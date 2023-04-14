b94415f961cf94e4a8a03084e31b3f1157f57db0
Rajendra Shivaraya <Rajendra.shivaraya@hotmail.com>
2023-04-13 17:46:48 -0700

    Converting the project into Spring Project

---
+++ JoyBuyEcomm_MultiModule/inventory/src/main/java/com/joybuy/InventoryApplication.java
@@ -0,0 +1,26 @@
+package com.joybuy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class InventoryApplication
+{
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(InventoryApplication.class, args);
+        displayAllBeans();
+    }
+
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for(String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
+    }
+}
